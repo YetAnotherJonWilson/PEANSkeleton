61e0c76f79eee34a1233bd5be1f3346ac8620ac2
Jon Wilson <jonwilsoncomedy@gmail.com>
2017-01-26 22:38:41 -0600

    adding pg session info

--- server.js
+++ server.js
@@ -4,30 +4,28 @@ var pg = require('pg');
 var bodyParser = require('body-parser');
 var index = require('./index');
 
+var connectionString = process.env.DATABASE_URL;
+var session = require('express-session');
 
 // See express session docs for information on the options: https://github.com/expressjs/session
-// var pgSession = require('connect-pg-simple')(session);
-// app.use(session({
-//     store: new pgSession({
-//         conString : connectionString
-//     }),
-//     secret: process.env.DB_SECRET,
-//     resave: false,
-//     cookie: {
-//         maxAge: 7 * 24 * 60 * 60 * 1000
-//     },
-//     secure : true,
-//     saveUninitialized: false
-// }));
+var pgSession = require('connect-pg-simple')(session);
+app.use(session({
+    store: new pgSession({
+        conString : connectionString
+    }),
+    secret: process.env.DB_SECRET,
+    resave: false,
+    cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    },
+    secure : true,
+    saveUninitialized: false
+}));
 
 //"configs"
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-
-
-
-
 
 //routes
 app.use('/', index);
