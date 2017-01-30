96777870d46f819351b44756ffe6931231c0404d
Jon Wilson <jonwilsoncomedy@gmail.com>
2017-01-29 23:48:29 -0600

    refactoring server and index to work with nodemon

--- server.js
+++ server.js
@@ -2,27 +2,8 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var bodyParser = require('body-parser');
-var index = require('./index');
+var index = require('./routes/index');
 
-// var connectionString = process.env.DATABASE_URL;
-// var session = require('express-session');
-//
-// // See express session docs for information on the options: https://github.com/expressjs/session
-// var pgSession = require('connect-pg-simple')(session);
-// app.use(session({
-//     store: new pgSession({
-//         conString : connectionString
-//     }),
-//     secret: process.env.DB_SECRET,
-//     resave: false,
-//     cookie: {
-//         maxAge: 7 * 24 * 60 * 60 * 1000
-//     },
-//     secure : true,
-//     saveUninitialized: false
-// }));
-
-//"configs"
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
